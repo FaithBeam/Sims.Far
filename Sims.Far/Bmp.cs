472943404e87328bbb42ccb6e44b580d3a45d216
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2020-06-29 16:11:27 -0400

    Add project files.

---
+++ Sims.Far/Bmp.cs
@@ -0,0 +1,9 @@
+﻿namespace Sims.Far
+{
+    public class Bmp
+    {
+        public int StartOffset { get; set; }
+        public int EndOffset { get; set; }
+        public string Path { get; set; }
+    }
+}
