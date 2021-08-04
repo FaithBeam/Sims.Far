bc9f46db75e16e7052f44568612754fa90574112
FaithBeam <pigeonhopper68@gmail.com>
2021-08-04 06:58:19 -0400

    Check throw ManifestEntryNotFoundException if filename is not found in far.

---
+++ Sims.Far.Exceptions/ManifestEntryNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sims.Far.Exceptions
+{
+    public class ManifestEntryNotFoundException : Exception
+    {
+        public ManifestEntryNotFoundException()
+        {
+        }
+
+        public ManifestEntryNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public ManifestEntryNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
